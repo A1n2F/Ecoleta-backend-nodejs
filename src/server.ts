532cd6f0f4e4d6ab60594f6dd1e26bc1eb57768f
A1n2F <alissonfferreira22@gmail.com>
2025-05-28 15:01:04 -0300

    Cadastro dos itens

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import path from "path";
 import routes from "./routes"
 
 const PORT = 3333;
@@ -7,5 +8,7 @@ const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(PORT, () => {console.log("Server is running!")})
