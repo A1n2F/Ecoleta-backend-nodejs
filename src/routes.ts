c5d4e9411638dd6f19a8c52069ac04974b3091da
A1n2F <alissonfferreira22@gmail.com>
2025-05-29 20:10:58 -0300

    Cadastro do ponto de coleta

--- src/routes.ts
+++ src/routes.ts
@@ -8,12 +8,33 @@ routes.get("/items", async (request, response) => {
 
     const serializedItems = items.map(item => {
         return {
+            id: item.id,
             title: item.title,
             image_url: `http://localhost:3333/uploads/${item.image}`
         }
     })
 
     response.json(serializedItems)
+})
+
+routes.post("/points", async (request, response) => {
+    const { name, email, telefone, latitude, longitude, city, uf, items } = request.body
+
+    const trx = await knex.transaction()
+
+    const insertedIds = await trx("points").insert({ image: "image-fake", name, email, telefone, latitude, longitude, city, uf })
+
+    const point_id = insertedIds[0]
+
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id
+        }
+    })
+    await trx("point_items").insert(pointItems)
+
+    response.json({ success: true })
 })
 
 export default routes
